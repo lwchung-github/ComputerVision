133ba9eb0c61524cb53ca94d5f8bdfd973741b5b
lwchung-github <u106029049@gap.kmu.edu.tw>
2021-07-26 14:13:37 +0800

    我嘗試修改

--- test.py
+++ test.py
@@ -3,8 +3,13 @@ import cv2
 img = cv2.imread('images/test1.jpg')
 
 # print(img) #B-G-R
-cv2.namedWindow('test', cv2.WINDOW_NORMAL)
+# cv2.namedWindow('test', cv2.WINDOW_NORMAL)
 cv2.imshow('test', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('test.png', img)
+
+k = cv2.waitKey(0)
+
+if k == 27:
+    cv2.destroyAllWindows()
+else:
+    cv2.imwrite('test.png', img)
+    cv2.destroyAllWindows()
