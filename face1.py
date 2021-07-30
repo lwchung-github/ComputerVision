e386efc9c6e0c8d703312caadb1b1ae4c664ec1c
lwchung-github <u106029049@gap.kmu.edu.tw>
2021-07-30 17:58:26 +0800

    0730

---
+++ face1.py
@@ -0,0 +1,20 @@
+from cvzone1 import FaceDetector
+import cv2
+
+detector = FaceDetector(model_selection=0)
+
+while True:
+    img = cv2.imread('./images/akb.jpg')
+    # success, img = cap.read()
+    img, bboxs = detector.findFaces(img)
+
+    if bboxs:
+        # bboxInfo - "id","bbox","score","center"
+        center = bboxs[0]["center"]
+        cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+
+    cv2.imshow("Image", img)
+    k= cv2.waitKey(1)
+    if k == 27:
+        break
+cv2.destroyAllWindows()    
